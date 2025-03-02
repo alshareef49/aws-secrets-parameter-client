7a18394871ce0d9ea8f6dd562f06ae0ff4ee55e6
Shareef Alam <alshareef4039@gmail.com>
2025-03-02 14:56:27 +0530

    Create AwsSecretParameterClientApplication.java

---
+++ src/main/java/com/aws/config/AwsSecretParameterClientApplication.java
@@ -0,0 +1,12 @@
+package com.aws.config;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSecretParameterClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AwsSecretParameterClientApplication.class, args);
+    }
+}
